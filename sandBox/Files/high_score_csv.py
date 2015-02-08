0e1837e7fd5346c1acfb25a5adafbb6b4fb2f2d9
The-Skas <skas@live.co.uk>
2015-02-08 13:19:39 +0000

    Sorting mutliple tuples.

--- sandBox/Files/high_score_csv.py
+++ sandBox/Files/high_score_csv.py
@@ -1,4 +1,5 @@
 import csv
+import pdb
 # All file modes in Python
 # -------
 # r for reading
@@ -9,20 +10,39 @@ import csv
 # wb+ writing a binary file
 # a+ opens for appending
 
-csvfile = open('names.csv','w')
+def writeFresh():
+	csvfile = open('names.csv','w')
 
-header_high_score = ["First Name", "Last Name", "Score"]
-user_scores = [
-		["Salman", "Khalifa", 130],
-		["Ali",    "Mulla",   139],
-		["Joe",	   "Biden",   503]
-	]
+	header_high_score = ["First Name", "Last Name", "Score"]
+	user_scores = [
+			["Salman", "Khalifa", 130],
+			["Ali",    "Mulla",   20],
+			["Joe",	   "Biden",   503],
+			["Dude",   "Wilson",   13]
+		]
 
-writer = csv.writer(csvfile)
+	writer = csv.writer(csvfile)
 
-# Writes the header
-writer.writerow(header_high_score)
-for x in user_scores:
-	print x
-	writer.writerow(x)
+	# Writes the header
+	writer.writerow(header_high_score)
+	for x in user_scores:
+		writer.writerow(x)
+
+def readFile():
+	csvfile = open('names.csv','r')
+	rd = csv.reader(csvfile)
+
+	values = list()
+	for row in rd:
+		values.append(row)
+
+	return values
+
+writeFresh()
+data = readFile()
+
+data = sorted(data[1::], key = lambda x : int( x[2]))
+
+print data
+
 
